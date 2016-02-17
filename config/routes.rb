19e6709d7d6973324426fee7ee4bfa26277a2b4a
nel <j.werner.nel@gmail.com>
2016-02-17 23:47:07 +0000

    Add edit, update, show and index listings.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
+  get 'articles/new', to: 'articles#new'
+
   resources :articles
 
   # Example of regular route:
