6ce554cb8c5cac44bf50ef1788cffa76033075cf
nel <j.werner.nel@gmail.com>
2016-03-07 22:45:42 +0000

    Add user listing and update show article view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -16,7 +21,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -26,8 +31,8 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
-  def show 
+
+  def show
     @user = User.find(params[:id])
   end
 
